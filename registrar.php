7d2dfddd86d3d671dbf4b0b234980b335b4697e5
PaoAFK <cguerrerosh@gmail.com>
2023-01-27 19:54:58 -0500

    .

--- registrar.php
+++ registrar.php
@@ -1,5 +1,5 @@
 <?php
-    //print_r($_POST);
+    //print_r($_POST);  asdasd
     if(empty($_POST["oculto"]) || empty($_POST["txtNombre"]) || empty($_POST["txtEdad"]) || empty($_POST["txtSigno"])){
         header('Location: index.php?mensaje=falta');
         exit();
